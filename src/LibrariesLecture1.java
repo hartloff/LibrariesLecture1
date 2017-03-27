0249846e957f1e76f15826e7bcb9e811b333f962
jessehartloff <hartloff@buffalo.edu>
2017-03-26 22:00:12 -0400

    initial commit

---
+++ src/LibrariesLecture1.java
@@ -0,0 +1,18 @@
+
+public class LibrariesLecture1 {
+
+
+    // 5 points
+    static cse115.math.NormalDistribution Q1(double mean, double standardDeviation) {
+        // return a new normal distribution with the provided mean and standard deviation.
+        // Use the NormalDistribution class in the provided library
+
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
